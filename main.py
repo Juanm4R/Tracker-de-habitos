667d71b3484788113704646b78d4b6a7bb3a6c16
sofia goldenberg <sgoldenberg31@gmail.com>
2025-09-05 14:14:18 -0300

    agregado eliminar habito

---
+++ main.py
@@ -0,0 +1,13 @@
+def eliminar_habito():
+    if not mostrar_habitos():
+        return
+    try:
+        num = int(input("Ingrese el número del hábito a eliminar: "))
+        if 1 <= num <= len(habitos):
+            nombre = habitos[num-1][0]
+            habitos.pop(num-1)
+            print(f"\n Hábito '{nombre}' eliminado.")
+        else:
+            print("\n Número inválido.")
+    except ValueError:
+        print("\n Debe ingresar un número.")
